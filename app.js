f6b5c150a4e79ce3242065a2ed5ecfe6a58eab8c
Alex Kashko <alexkrond@yahoo.com>
2020-01-28 21:29:34 +0300

    Добавлен роут для получения тарифов

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const app = express();
 
-app.use(express.static('public'));
+app.get('/getTariffs', async (req, res) => {
+  const response = await fetch('http://frontend.usdev.ru/tarif.csv')
+      .catch(err => {
+        console.log(err);
+        res.status(200).send({'status': false, 'msg': err.message});
+      });
+  let tariffs = await response.text();
 
+  tariffs = tariffs.split('\n').map(t => t.split(',')).filter(t => t[0] !== '');
+  const headers = tariffs.shift();
+
+  tariffs = tariffs.map(t => {
+    const r = {};
+    headers.forEach((h, i) => r[h] = t[i]);
+    return r;
+  });
+
+  res.send({'status': 'OK', tariffs});
+});
+
+app.use(express.static('public'));
 app.use((req, res) => {
   res.status(404).send('404. Страница не существует');
 });
